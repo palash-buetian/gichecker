47d80d3b65614b6fa907c7ed6a6d5df83ae0c5e9
Palash Mondal <paalsh.buet06@gmail.com>
2022-08-12 18:29:19 +0600

    comment out

--- ajax.php
+++ ajax.php
@@ -18,7 +18,7 @@ if (isset($_POST['dag']))
         $query = "SELECT * FROM dag WHERE (sa_dag LIKE '{$dag}%' OR bs_dag LIKE '{$dag}%')  ORDER BY CASE WHEN (sa_dag='{$dag}' OR bs_dag='{$dag}') THEN 1 WHEN (sa_dag LIKE '{$dag}%' OR bs_dag LIKE '{$dag}%') THEN 2 ELSE 3 END LIMIT 20";
     }
 
-  print_r($query);
+  //print_r($query);
 
     $result = mysqli_query($conn, $query);
 
